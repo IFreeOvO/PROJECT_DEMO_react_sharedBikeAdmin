af879d3788b593d2f2b99db1434583f6958a0677
chenzihao <278199196@qq.com>
2019-08-25 16:26:53 +0800

    修复--课程进度9-6中订单详情页布局问题

--- src/pages/order/detail.js
+++ src/pages/order/detail.js
@@ -1,48 +1,46 @@
 import React from 'react'
-import {
-  Card,
-} from 'antd'
+import { Card } from 'antd'
 import axios from './../../axios/index'
 import './detail.less'
 
-
 class Order extends React.Component {
-
   state = {
     orderInfo: {}
   }
 
   componentDidMount() {
     let orderId = this.props.match.params.orderId
-    if(orderId) {
+    if (orderId) {
       this.getDetailInfo(orderId)
     }
   }
 
-  getDetailInfo = (orderId) => {
-    axios.ajax({
-      url: '/order/detail',
-      data: {
-        params: {
-          orderId
+  getDetailInfo = orderId => {
+    axios
+      .ajax({
+        url: '/order/detail',
+        data: {
+          params: {
+            orderId
+          }
         }
-      }
-    }).then(res => {
-      if(res.code === '0') {
-        console.log('订单详情', res)
-        this.setState({
-          orderInfo: res.result
-        })
-        this.renderMap(res.result)
-      }
-    })
+      })
+      .then(res => {
+        if (res.code === '0') {
+          console.log('订单详情', res)
+          this.setState({
+            orderInfo: res.result
+          })
+          this.renderMap(res.result)
+        }
+      })
   }
 
   // 渲染地图
-  renderMap = (result) => {
+  renderMap = result => {
     this.map = new window.BMap.Map('orderDetailMap')
     // this.map.centerAndZoom('北京', 11)
-     // 添加地图控件
+    // 添加地图控件
     this.addMapControl()
     // 调用路线图绘制方法
     this.drawBikeRoute(result.position_list)
@@ -52,39 +50,53 @@ class Order extends React.Component {
 
   // 添加地图控件
   addMapControl = () => {
-    let map = this.map 
-    map.addControl(new window.BMap.ScaleControl({anchor : window.BMAP_ANCHOR_TOP_RIGHT}))
-    map.addControl(new window.BMap.NavigationControl({anchor : window.BMAP_ANCHOR_TOP_RIGHT}))
+    let map = this.map
+    map.addControl(
+      new window.BMap.ScaleControl({ anchor: window.BMAP_ANCHOR_TOP_RIGHT })
+    )
+    map.addControl(
+      new window.BMap.NavigationControl({
+        anchor: window.BMAP_ANCHOR_TOP_RIGHT
+      })
+    )
   }
 
   // 绘制路线图
-  drawBikeRoute = (positionList) => {
+  drawBikeRoute = positionList => {
     // let map = this.map
     let startPoint = ''
     let endPoint = ''
 
-    if(positionList.length > 0) {
+    if (positionList.length > 0) {
       let first = positionList[0]
       let last = positionList[positionList.length - 1]
       startPoint = new window.BMap.Point(first.lon, first.lat)
-      let startIcon = new window.BMap.Icon('/assets/start_point.png', new window.BMap.Size(36, 42), {
-        imageSize: new window.BMap.Size(36, 42),
-        anchor: new window.BMap.Size(36, 42)
-      })
-      let startMarker = new window.BMap.Marker(startPoint, {icon: startIcon})
+      let startIcon = new window.BMap.Icon(
+        '/assets/start_point.png',
+        new window.BMap.Size(36, 42),
+        {
+          imageSize: new window.BMap.Size(36, 42),
+          anchor: new window.BMap.Size(36, 42)
+        }
+      )
+      let startMarker = new window.BMap.Marker(startPoint, { icon: startIcon })
       this.map.addOverlay(startMarker)
 
       endPoint = new window.BMap.Point(last.lon, last.lat)
-      let endIcon = new window.BMap.Icon('/assets/end_point.png', new window.BMap.Size(36, 42), {
-        imageSize: new window.BMap.Size(36, 42),
-        anchor: new window.BMap.Size(36, 42)
-      })
-      let endMarker = new window.BMap.Marker(endPoint, {icon: endIcon})
+      let endIcon = new window.BMap.Icon(
+        '/assets/end_point.png',
+        new window.BMap.Size(36, 42),
+        {
+          imageSize: new window.BMap.Size(36, 42),
+          anchor: new window.BMap.Size(36, 42)
+        }
+      )
+      let endMarker = new window.BMap.Marker(endPoint, { icon: endIcon })
       this.map.addOverlay(endMarker)
 
       // 连接路线图
       let trackPoint = []
-      for(let i = 0; i<positionList.length; i++) {
+      for (let i = 0; i < positionList.length; i++) {
         let point = positionList[i]
         trackPoint.push(new window.BMap.Point(point.lon, point.lat))
       }
@@ -99,12 +111,12 @@ class Order extends React.Component {
   }
 
   // 绘制服务区
-  drawServiceArea = (position_list) => {
+  drawServiceArea = position_list => {
     let trackPoint = []
-      for(let i = 0; i<position_list.length; i++) {
-        let point = position_list[i]
-        trackPoint.push(new window.BMap.Point(point.lon, point.lat))
-      }
+    for (let i = 0; i < position_list.length; i++) {
+      let point = position_list[i]
+      trackPoint.push(new window.BMap.Point(point.lon, point.lat))
+    }
 
     let polygon = new window.BMap.Polygon(trackPoint, {
       strokeColor: '#CE0000',
@@ -121,56 +133,60 @@ class Order extends React.Component {
     return (
       <div>
         <Card>
-          <div id="orderDetailMap" className="order-map">
-            <div className="detail-items">
-              <div className="item-title">基础信息</div>
-              <ul className="detail-form">
-                <li>
-                  <div className="detail-form-left">用车模式</div>
-                  <div className="detail-form-content" >{info.mode === 1? '服务器': '停车点' }</div>
-                </li>
+          <div id="orderDetailMap" className="order-map" />
 
-                <li>
-                  <div className="detail-form-left">订单编号</div>
-                  <div className="detail-form-content" >{info.order_sn}</div>
-                </li>
+          <div className="detail-items">
+            <div className="item-title">基础信息</div>
+            <ul className="detail-form">
+              <li>
+                <div className="detail-form-left">用车模式</div>
+                <div className="detail-form-content">
+                  {info.mode === 1 ? '服务器' : '停车点'}
+                </div>
+              </li>
 
-                <li>
-                  <div className="detail-form-left">车辆编号</div>
-                  <div className="detail-form-content" >{info.bike_sn}</div>
-                </li>
+              <li>
+                <div className="detail-form-left">订单编号</div>
+                <div className="detail-form-content">{info.order_sn}</div>
+              </li>
 
-                <li>
-                  <div className="detail-form-left">用户姓名</div>
-                  <div className="detail-form-content" >{info.user_name}</div>
-                </li>
+              <li>
+                <div className="detail-form-left">车辆编号</div>
+                <div className="detail-form-content">{info.bike_sn}</div>
+              </li>
 
-                <li>
-                  <div className="detail-form-left">手机号码</div>
-                  <div className="detail-form-content" >{info.mobile}</div>
-                </li>
-              </ul>
-            </div>
+              <li>
+                <div className="detail-form-left">用户姓名</div>
+                <div className="detail-form-content">{info.user_name}</div>
+              </li>
 
-            <div className="detail-items">
-              <div className="item-title">行车轨迹</div>
-              <ul className="detail-form">
-                <li>
-                  <div className="detail-form-left">行程起点</div>
-                  <div className="detail-form-content" >{info.start_location}</div>
-                </li>
+              <li>
+                <div className="detail-form-left">手机号码</div>
+                <div className="detail-form-content">{info.mobile}</div>
+              </li>
+            </ul>
+          </div>
 
-                <li>
-                  <div className="detail-form-left">行程终点</div>
-                  <div className="detail-form-content" >{info.end_location}</div>
-                </li>
+          <div className="detail-items">
+            <div className="item-title">行车轨迹</div>
+            <ul className="detail-form">
+              <li>
+                <div className="detail-form-left">行程起点</div>
+                <div className="detail-form-content">{info.start_location}</div>
+              </li>
 
-                <li>
-                  <div className="detail-form-left">行驶里程</div>
-                  <div className="detail-form-content" >{info.distance/1000}公里</div>
-                </li>
-              </ul>
-            </div>
+              <li>
+                <div className="detail-form-left">行程终点</div>
+                <div className="detail-form-content">{info.end_location}</div>
+              </li>
+
+              <li>
+                <div className="detail-form-left">行驶里程</div>
+                <div className="detail-form-content">
+                  {info.distance / 1000}公里
+                </div>
+              </li>
+            </ul>
           </div>
         </Card>
       </div>
